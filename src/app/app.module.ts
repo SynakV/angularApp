c5438d4c86b08aa83c87673c45ba16af6ff725c3
Vasyl Sunak <sunack16@gmail.com>
2018-10-11 23:34:22 +0300

    lec21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { from } from 'rxjs';
+import { ServersComponent } from './servers/servers.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ManualComponent } from './manualComponent/manual.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    MyComponentComponent,
+    ManualComponent
   ],
   imports: [
     BrowserModule
