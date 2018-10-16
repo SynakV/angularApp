f911b0f9f242773bed40d0e293ccd7649ca89c81
Vasyl Sunak <vasyl.sunak@edvantis.com>
2018-10-16 18:20:29 +0300

    lec73

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 import { from } from 'rxjs';
-import { ServersComponent } from './servers/servers.component';
-import { MyComponentComponent } from './my-component/my-component.component';
-import { ManualComponent } from './manualComponent/manual.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    MyComponentComponent,
-    ManualComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
